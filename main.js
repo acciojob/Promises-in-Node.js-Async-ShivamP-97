717a0138375dfe7d28cf0abf116b6083df289fbc
= <shivampathak19972@gmail.com>
2026-01-13 23:17:51 +0530

    Test

--- main.js
+++ main.js
@@ -2,16 +2,18 @@ const fs = require('fs');
 
 const filePath = process.argv[2];
 
+// Guard for no file path
 if (!filePath) {
   console.log("Error reading file");
   process.exit(0);
 }
 
-function readFileAsync(filePath) {
-  return new Promise((resolve, reject) => {
-    fs.readFile(filePath, 'utf8', (err, data) => {
+// Read file asynchronously
+function readFileAsync(path) {
+  return new Promise((resolve) => {
+    fs.readFile(path, 'utf8', (err, data) => {
       if (err) {
-        reject("Error reading file");
+        resolve(null); // Resolve null instead of rejecting
         return;
       }
       resolve(data);
@@ -19,14 +21,17 @@ function readFileAsync(filePath) {
   });
 }
 
+// Modify text: uppercase + reverse
 function modifyText(text) {
-  return new Promise((resolve) => {
-    const modified = text.toUpperCase().split('').reverse().join('');
-    resolve(modified);
-  });
+  if (!text) return ""; // In case file missing
+  return text.toUpperCase().split("").reverse().join("");
 }
 
-readFileAsync(filePath)
-  .then((data) => modifyText(data))
-  .then((modifiedText) => console.log(modifiedText))
-  .catch((error) => console.log(error));
+// Run
+readFileAsync(filePath).then((data) => {
+  if (!data) {
+    console.log("Error reading file");
+  } else {
+    console.log(modifyText(data));
+  }
+});
